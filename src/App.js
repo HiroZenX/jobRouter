0f29722fdd94ca64f21c05fb36fff71a23331696
Hiro <hirobaymax4321@gmail.com>
2022-12-20 17:00:10 +0530

    New files added, Home and About component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import {
+  createBrowserRouter, 
+  Route, 
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom';
+
+// pages
+import Home from './pages/Home';
+import About from './pages/About';
+
+// Layouts
+import RootLayout from './layouts/RootLayout';
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path='about' element={<About />} />
+      
+    </Route>
+  )
+)
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello wORLd</h1>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
